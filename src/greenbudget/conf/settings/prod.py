9d908bfc0496bffa6f3ffc3a9ab4ee553c7b081e
nickmflorin <nickmflorin@gmail.com>
2021-03-17 20:01:13 -0400

    Change cors

--- src/greenbudget/conf/settings/prod.py
+++ src/greenbudget/conf/settings/prod.py
@@ -10,6 +10,7 @@ ENVIRONMENT = Environments.PROD
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_REGEX_WHITELIST = (
     r'^(https?://)?([\w\.-]*?)\.greenbudget\.io$',
+    r'^(https?://)?([\w\.-]*?)3.88.164.226\.io$',
 )
 
 ALLOWED_HOSTS = [
