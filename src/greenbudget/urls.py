d41a5ca31a0aa4720010fd85a0b9e8b4ed8ac4db
nickmflorin <nickmflorin@gmail.com>
2021-03-17 21:30:02 -0400

    Add root url

--- src/greenbudget/urls.py
+++ src/greenbudget/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('v1/', include('greenbudget.app.urls'))
+    path('v1/', include('greenbudget.app.urls')),
+    path('', lambda request: JsonResponse({'status': '200'})),
 ]
