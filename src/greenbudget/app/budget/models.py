a8cae9c649742d3a45dcd023a20b26297e4023e0
nickmflorin <nickmflorin@gmail.com>
2021-04-21 19:40:37 -0400

    Ensure uniqueness with trash state

--- src/greenbudget/app/budget/models.py
+++ src/greenbudget/app/budget/models.py
@@ -28,7 +28,9 @@ class BaseBudget(PolymorphicModel):
         ordering = ('-updated_at', )
         verbose_name = "Base Budget"
         verbose_name_plural = "Base Budgets"
-        unique_together = (('created_by', 'name', 'polymorphic_ctype_id'), )
+        unique_together = (
+            ('created_by', 'name', 'polymorphic_ctype_id', 'trash'),
+        )
 
     def __str__(self):
         return "<{cls} id={id}, name={name}>".format(
