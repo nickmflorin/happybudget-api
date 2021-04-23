7d4e43cc48e69215278706353289eb0847c246e1
nickmflorin <nickmflorin@gmail.com>
2021-04-23 12:01:18 -0400

    Add base64 image field for future use

--- src/greenbudget/lib/rest_framework_utils/fields.py
+++ src/greenbudget/lib/rest_framework_utils/fields.py
@@ -1,3 +1,8 @@
+import base64
+import imghdr
+import uuid
+
+from django.core.files.base import ContentFile
 from rest_framework import serializers
 
 
@@ -34,3 +39,30 @@ class ModelChoiceField(serializers.ChoiceField):
         if value is not None:
             return {'id': value, 'name': self.choices[value]}
         return value
+
+
+class Base64ImageField(serializers.ImageField):
+    """
+    A :obj:`rest_framework.serializers.ImageField` field that allows image
+    uploads via raw POST data.  It uses base64 for encoding/decoding the
+    contents of the file.
+    """
+
+    def to_internal_value(self, data):
+        if isinstance(data, str):
+            if 'data:' in data and ';base64,' in data:
+                header, data = data.split(';base64,')
+            try:
+                decoded_file = base64.b64decode(data)
+            except TypeError:
+                self.fail('invalid_image')
+            file_name = str(uuid.uuid4())[:12]
+            file_extension = self.get_file_extension(file_name, decoded_file)
+            complete_file_name = "%s.%s" % (file_name, file_extension, )
+            data = ContentFile(decoded_file, name=complete_file_name)
+
+        return super(Base64ImageField, self).to_internal_value(data)
+
+    def get_file_extension(self, file_name, decoded_file):
+        extension = imghdr.what(file_name, decoded_file)
+        return "jpg" if extension == "jpeg" else extension
