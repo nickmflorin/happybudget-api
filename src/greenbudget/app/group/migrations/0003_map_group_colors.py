bec4fe2c3f33d3a7a1f1fb21925a7ce8681f162b
nickmflorin <nickmflorin@gmail.com>
2021-05-03 09:33:00 -0400

    Fix second migration bug

--- src/greenbudget/app/group/migrations/0003_map_group_colors.py
+++ src/greenbudget/app/group/migrations/0003_map_group_colors.py
@@ -1,8 +1,11 @@
+from django.contrib.contenttypes.models import ContentType
 from django.db import migrations
 
 def map_group_colors(apps, schema_editor):
     Group = apps.get_model('group', 'Group')
     Color = apps.get_model('tagging', 'Color')
+
+    ct = ContentType.objects.get_for_model(Group)
 
     for group in Group.objects.all():
         if group.color is None:
@@ -11,7 +14,7 @@ def map_group_colors(apps, schema_editor):
             color_obj = Color.objects.get(code=group.color)
         except Color.DoesNotExist:
             color_obj = Color.objects.create(code=group.color)
-        color_obj.content_types.add(Group)
+        color_obj.content_types.add(ct)
 
 
 class Migration(migrations.Migration):
