aa8b8ac150f2e87363831b3bf529b07e96142466
nickmflorin <nickmflorin@gmail.com>
2021-05-03 09:35:40 -0400

    Try this

--- src/greenbudget/app/group/migrations/0003_map_group_colors.py
+++ src/greenbudget/app/group/migrations/0003_map_group_colors.py
@@ -1,9 +1,9 @@
-from django.contrib.contenttypes.models import ContentType
 from django.db import migrations
 
 def map_group_colors(apps, schema_editor):
     Group = apps.get_model('group', 'Group')
     Color = apps.get_model('tagging', 'Color')
+    ContentType = apps.get_model('contenttypes', 'ContentType')
 
     ct = ContentType.objects.get_for_model(Group)
 
@@ -15,6 +15,7 @@ def map_group_colors(apps, schema_editor):
         except Color.DoesNotExist:
             color_obj = Color.objects.create(code=group.color)
         color_obj.content_types.add(ct)
+        color_obj.save()
 
 
 class Migration(migrations.Migration):
