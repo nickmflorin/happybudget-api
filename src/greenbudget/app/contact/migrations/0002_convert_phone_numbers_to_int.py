6c3e42379235a5a3540a539e68b5e4238e62d353
nickmflorin <nickmflorin@gmail.com>
2021-06-07 12:54:45 -0400

    Fix migration bug

--- src/greenbudget/app/contact/migrations/0002_convert_phone_numbers_to_int.py
+++ src/greenbudget/app/contact/migrations/0002_convert_phone_numbers_to_int.py
@@ -6,8 +6,10 @@ def forwards_func(apps, schema_editor):
     Contact = apps.get_model("contact", "Contact")
 
     for obj in Contact.objects.all():
-        obj.phone_number = re.sub("[^0-9]", "", obj.phone_number)
-        obj.save()
+        if obj.phone_number is not None \
+                and not isinstance(obj.phone_number, int):
+            obj.phone_number = re.sub("[^0-9]", "", obj.phone_number)
+            obj.save()
 
 
 class Migration(migrations.Migration):
