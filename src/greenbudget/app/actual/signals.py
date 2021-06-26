2723632c25086ebc9b8a6564f005a24005af9e13
nickmflorin <nickmflorin@gmail.com>
2021-06-26 12:08:17 -0400

    Fix bug deleting sub accounts

--- src/greenbudget/app/actual/signals.py
+++ src/greenbudget/app/actual/signals.py
@@ -2,6 +2,7 @@ from django import dispatch
 from django.db import models
 
 from greenbudget.app import signals
+from greenbudget.app.subaccount.models import BudgetSubAccount
 from greenbudget.app.subaccount.signals import reactualize_subaccount
 
 from .models import Actual
@@ -10,10 +11,18 @@ from .models import Actual
 @dispatch.receiver(signals.post_create, sender=Actual)
 @dispatch.receiver(models.signals.post_delete, sender=Actual)
 def actual_created_or_deleted(instance, **kwargs):
-    if instance.subaccount is not None:
+    try:
+        subaccount = instance.subaccount
+    except BudgetSubAccount.DoesNotExist:
+        # This can happen when this delete is triggered from the deletion of
+        # a BudgetSubAccount - the BudgetSubAccount will be deleted, and the
+        # on_delete=models.CASCADE will trigger a delete of the Actual - but
+        # the BudgetSubAccount associated with the Actual is already gone.
+        pass
+    else:
         reactualize_subaccount.send(
-            instance=instance.subaccount,
-            sender=type(instance.subaccount),
+            instance=subaccount,
+            sender=type(subaccount),
         )
 
 
