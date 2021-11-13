b97c72baa8b5c5518412334698787aa28f115c41
nickmflorin <nickmflorin@gmail.com>
2021-11-12 20:58:57 -0500

    Removing some dead code

--- src/greenbudget/lib/drf/request.py
+++
@@ -1,6 +0,0 @@
-def is_truthy(value):
-    return value in (1, 'True', 'true', True)
-
-
-def parse_boolean_query_params(request, *params):
-    return {k: is_truthy(request.GET[k]) for k in params if k in request.GET}
