effd3e6434325e3183707fa7445661266677bd21
nickmflorin <nickmflorin@gmail.com>
2021-11-18 12:19:19 -0500

    Fix bug

--- src/greenbudget/app/actual/signals.py
+++ src/greenbudget/app/actual/signals.py
@@ -2,12 +2,14 @@ from django import dispatch
 from django.db import models
 
 from greenbudget.app import signals
+from greenbudget.app.budget.cache import budget_actuals_cache
 
 from .models import Actual
 
 
 @dispatch.receiver(signals.post_delete, sender=Actual)
 def actual_deleted(instance, **kwargs):
+    budget_actuals_cache.invalidate(instance)
     if instance.owner is not None:
         Actual.objects.reactualize_owner(instance, deleting=True)
 
