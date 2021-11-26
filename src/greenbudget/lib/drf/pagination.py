3ff3ad9b7e3600c47baaa938c0cf037d1ac81132
Nick Florin <nickmflorin@gmail.com>
2021-11-26 17:43:53 -0500

    remove dead code

--- src/greenbudget/lib/drf/pagination.py
+++ src/greenbudget/lib/drf/pagination.py
@@ -1,58 +1,5 @@
 from collections import OrderedDict
-import functools
-
-from django.db.models import QuerySet
 from rest_framework import pagination, response
-
-
-def paginate_action(serializer_cls=None):
-    """
-    Decorator for `rest_framework.decorators.action` endpoints that allows the
-    view's pagination to be applied to the queryset returned by the action
-    endpoint.
-
-    The decorated method must either return a queryset, a list of instances
-    or a tuple of a queryset/list of instances and the serializer class to
-    be used.
-
-    Example:
-    -------
-    In the below example, we return a filtered queryset from the `custom_action`
-    action and decorate the method with `paginate_action` so that the queryset
-    will be paginated based on the pagination scheme for `CustomViewSet`.
-
-    class CustomViewSet(...):
-
-        @decorators.action(detail=False, methods=["GET"])
-        @paginate_action
-        def custom_action(self, request, *args, **kwargs):
-            qs = self.get_queryset()
-            qs = qs.filter(...)
-            return qs
-    """
-    def decorator(func):
-        @functools.wraps(func)
-        def inner(instance, *args, **kwargs):
-            qs = func(instance, *args, **kwargs)
-
-            serializer_klass = serializer_cls
-            if serializer_klass is None:
-                serializer_klass = instance.get_serializer
-
-            assert isinstance(
-                qs, (list, QuerySet)), (
-                    "The `paginate_action` decorator must decorate a method "
-                    "that returns a queryset or a list.")
-
-            page = instance.paginate_queryset(qs)
-            if page is not None:
-                serializer = serializer_klass(page, many=True)
-                return instance.get_paginated_response(serializer.data)
-
-            serializer = serializer_klass(qs, many=True)
-            return response.Response(serializer.data)
-        return inner
-    return decorator
 
 
 class Pagination(pagination.PageNumberPagination):
