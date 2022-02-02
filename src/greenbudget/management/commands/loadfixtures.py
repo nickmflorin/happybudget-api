8a5ba73bf22a73833d0d1b199f2d401c49de6bf5
Nick Florin <nickmflorin@gmail.com>
2022-02-01 21:42:45 -0500

    Add decorator to loadfixtures

--- src/greenbudget/management/commands/loadfixtures.py
+++ src/greenbudget/management/commands/loadfixtures.py
@@ -1,9 +1,13 @@
 from django.core import management
 from django.conf import settings
+
 from greenbudget.management.base import CustomCommand
+
+from greenbudget.app import cache
 
 
 class Command(CustomCommand):
+    @cache.disable()
     def handle(self, **options):
         for fixture in settings.FIXTURES:
             management.call_command("loaddata", fixture)
