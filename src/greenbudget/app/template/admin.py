8fa8dd1b8fd1b5ef59fa98d019e9e9aab69213ee
Nick Florin <nickmflorin@gmail.com>
2022-02-07 19:56:16 -0500

    Updating template admin

--- src/greenbudget/app/template/admin.py
+++ src/greenbudget/app/template/admin.py
@@ -1,5 +1,9 @@
+import nested_admin
+
 from django import forms
 from django.contrib import admin
+
+from greenbudget.app.account.models import TemplateAccount
 
 from .models import Template
 
@@ -7,13 +11,19 @@ from .models import Template
 class TemplateAdminForm(forms.ModelForm):
     class Meta:
         model = Template
-        fields = '__all__'
+        fields = ('name', 'image')
 
 
-class TemplateAdmin(admin.ModelAdmin):
-    list_display = (
-        "name", "created_at", "updated_at", "created_by", "community")
+class AccountInline(nested_admin.NestedStackedInline):
+    model = TemplateAccount
+    sortable_field_name = "identifier"
+    fields = ('identifier', 'description', 'markups', 'group')
+
+
+class TemplateAdmin(nested_admin.NestedModelAdmin):
+    list_display = ("name", "created_at", "updated_at", "created_by")
     form = TemplateAdminForm
+    inlines = [AccountInline]
 
 
 admin.site.register(Template, TemplateAdmin)
