4442f77b1ace071548459fbf8cbc8e908cf418a7
Nick Florin <nickmflorin@gmail.com>
2022-02-18 18:08:24 -0500

    turn off cache config

--- src/greenbudget/conf/settings/base.py
+++ src/greenbudget/conf/settings/base.py
@@ -279,21 +279,21 @@ CACHE_ENABLED = False
 CACHE_LOCATION = f"redis://{ELASTICACHE_ENDPOINT}/0"
 CACHE_EXPIRY = 5 * 60 * 60
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': CACHE_LOCATION,
-        'OPTIONS': {
-            'REDIS_CLIENT_CLASS': 'rediscluster.RedisCluster',
-            'CONNECTION_POOL_CLASS': (
-                'rediscluster.connection.ClusterConnectionPool'),
-            'CONNECTION_POOL_KWARGS': {
-                # AWS ElastiCache has configuration commands disabled.
-                'skip_full_coverage_check': True
-            }
-        }
-    }
-}
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django_redis.cache.RedisCache',
+#         'LOCATION': CACHE_LOCATION,
+#         'OPTIONS': {
+#             'REDIS_CLIENT_CLASS': 'rediscluster.RedisCluster',
+#             'CONNECTION_POOL_CLASS': (
+#                 'rediscluster.connection.ClusterConnectionPool'),
+#             'CONNECTION_POOL_KWARGS': {
+#                 # AWS ElastiCache has configuration commands disabled.
+#                 'skip_full_coverage_check': True
+#             }
+#         }
+#     }
+# }
 
 FIXTURES = [
     'colors.json',
