0d77e50debfde76c76e57c869162114cccb3acfb
Nick Florin <nickmflorin@gmail.com>
2022-03-29 15:55:13 -0400

    Adding docstrings

--- src/greenbudget/app/budgeting/models.py
+++ src/greenbudget/app/budgeting/models.py
@@ -33,6 +33,11 @@ TreeDomainModelMap = {
 
 
 class BudgetTree:
+    """
+    Manages the related :obj:`BaseBudget`, :obj:`Account` and :obj:`SubAccount`
+    instances that are related to one another with behavior that mimics that
+    of a :obj:`set`.
+    """
     def __init__(self, budgets=None, accounts=None, subaccounts=None):
         self._budgets = ensure_iterable(budgets, cast=set)
         self._accounts = ensure_iterable(accounts, cast=set)
@@ -76,9 +81,62 @@ class BudgetTree:
 
 
 def children_method_handler(func):
+    """
+    Decorates a methods on a :obj:`BaseBudget`, :obj:`Account` or
+    :obj:`SubAccount` that accept a `children` argument and perform calculations
+    based on the accumulation of values of the children of the instance whose
+    method this decorates.
+
+    Exposed Parameters:
+    ------------------
+    The decorator will expose the following parameters on the method:
+
+    children: :obj:`django.db.models.QuerySet` or an iterable (optional)
+        The children that should be used to perform the calculation.  If
+        omitted, the `children` will be determined from a database query
+        of the related models on the instance that are defined to be the
+        children.
+
+        Default: None
+
+    children_to_delete: :obj:`list`, :obj:`tuple` or another iterable (optional)
+        A iterable of IDs associated with the children of the instance that are
+        going to be deleted.  If provided, the children associated with these
+        IDs will be excluded from the calculation.
+
+        Default: None
+
+    unsaved_children: :obj:`list` or :obj:`tuple` or another iterable
+        An iterable of children instances that have been updated but not yet
+        persisted to the database.  If provided, the children that are either
+        explicitly provided or determined from a database query will be
+        supplemented with the updated forms of the instances dictated by this
+        parameter.
+
+        This is important when performing bulk changes, as there are cases where
+        we want to reperform calculations on models but have not yet saved
+        children of the model instance that were updated to the database yet.
+
+        Note:  Unsaved does not mean never-saved, the unsaved children should
+               all have associated IDs.
+
+        Default: None
+
+    All of the exposed parameters are combined to a single `children` parameter
+    that the method is then called with.
+    """
     @functools.wraps(func)
     def inner(instance, *args, **kwargs):
+        # Any potential children that are going to be deleted but have yet to
+        # be deleted from the database.  These children will be exclued from
+        # the children passed into the method.
         children_to_delete = kwargs.pop('children_to_delete', []) or []
+
+        # Any potential children to the instance that have been altered but not
+        # yet persisted to the database.  These instances will supplement the
+        # children passed into the method such that the children passed into the
+        # method contain up to date information - even if it hasn't persisted
+        # to the database yet.
         unsaved_children = kwargs.pop('unsaved_children', []) or []
 
         # We have to be careful with empty lists here, because an empty list
@@ -88,6 +146,10 @@ def children_method_handler(func):
             if args:
                 children = args[0]
             else:
+                # Perform the database query to get the instance children.
+                # Note that these children will not be representative of any
+                # unsaved changes until they are supplemented with the
+                # `unsaved_children`.
                 children = instance.children.all()
 
         assert all([
