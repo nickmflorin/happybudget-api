4b73cfdf0a61ac3936807c92010e4963c8e21420
Nick Florin <nickmflorin@gmail.com>
2022-05-22 13:57:43 -0400

    Disabling admin actions for email config

--- src/happybudget/app/user/admin.py
+++ src/happybudget/app/user/admin.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.conf import settings
 
 from happybudget import harry
 
@@ -13,6 +14,9 @@ class UserAdminForm(forms.ModelForm):
 
 
 class UserAdmin(harry.HarryModelAdmin):
+    # Note: We may have to remove the `billing_status` and `product_id` fields
+    # from the list_display because it will submit an API request to Stripe for
+    # each user displayed in the table.
     base_list_display = (
         "first_name", "last_name", "email", "created_at", "last_login",
         "is_staff", "is_superuser", "billing_status", "product_id")
@@ -29,18 +33,23 @@ class UserAdmin(harry.HarryModelAdmin):
             )
         }),
     )
-    row_actions = [
-        harry.RowAction(
-            name='send_email_verification',
-            title='Send Email Verification',
-            disabled=lambda user: user.email_is_verified
-        ),
-        harry.RowAction(
-            name='send_forgot_password',
-            title='Send Forgot Password'
-        )
-    ]
     form = UserAdminForm
+
+    @property
+    def row_actions(self):
+        actions = []
+        if settings.EMAIL_ENABLED:
+            actions += [harry.RowAction(
+                name='send_forgot_password',
+                title='Send Forgot Password'
+            )]
+            if settings.EMAIL_VERIFICATION_ENABLED:
+                actions += [harry.RowAction(
+                    name='send_email_verification',
+                    title='Send Email Verification',
+                    disabled=lambda user: user.email_is_verified
+                )]
+        return actions
 
     def send_email_verification(self, request, instance_id):
         user = User.objects.get(pk=instance_id)
